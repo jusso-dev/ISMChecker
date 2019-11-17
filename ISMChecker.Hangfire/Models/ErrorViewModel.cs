e777010ab8a2f066d2dd49b8bc92268e37e81879
Justin Middler <justin.middler@njoysecurity.com>
2019-11-17 14:17:50 +1100

    Added hangfire

---
+++ ISMChecker.Hangfire/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ISMChecker.Hangfire.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
